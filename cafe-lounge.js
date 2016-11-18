7b8e90c454002fa39de2f83615a83d43b9a8bae3
Austin Fish <TurqDevDesign@Gmail.com>
2016-11-18 15:03:07 -0500

    Made progress on some new methods

--- cafe-lounge.js
+++ cafe-lounge.js
@@ -2,6 +2,111 @@
 var cafe = {
         version: "0.1",
         author: "Austin Fish",
+        html: '',
+        is: {
+            /**
+             * [what description]
+             * @param  {*} item        Item to check
+             * @return {string|array}  Single string or array of matching types.
+             *                         Matchable types include node, string,
+             *                         number, boolean, object, array,undefined,
+             *                         null.
+             */
+            what: function(item){
+
+            },
+            /**
+             * Check if passed item is a node
+             *
+             * @param  {*} item                   Item to check against specified type
+             * @param  {number|string} [inDOM=2]  Three values: 0, 1, or 2.
+             *                                    0 = Check that item is node, but return
+             *                                        false if it is in the DOM.
+             *                                    1 = Check that item is node, but return
+             *                                        false if it is NOT in the DOM.
+             *                                    2 = Only check if the item is a node,
+             *                                        doesn't matter where it's stored.
+             * @param  {string} [tag=false]       Supply tag name to check for specific type.
+             *                                    For instance cafe.is.node(item, 'div'); will
+             *                                    return true only if item is a div.
+             * @return {boolean}                  Returns true if item is of specified type
+             */
+            node: function(item, inDOM = 2, tag = false){
+                if(item.nodeType){
+                    let found = cafe.html.contains(item);
+                    if(inDOM == 0 && found){
+                        return false;
+                    }
+                    if(inDOM == 1 && !found){
+                        return false;
+                    }
+                    if(tag){
+                        if(item.tagName.toLowerCase() === tag){
+                            return true;
+                        }
+                        return false
+                    }
+                    return true
+                }
+                return false;
+            },
+            /**
+             * Check if passed item is a string
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            string: function(item){
+
+            },
+            /**
+             * Check if passed item is a number
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            number: function(item){
+
+            },
+            /**
+             * Check if passed item is a boolean
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            boolean: function(item){
+
+            },
+            /**
+             * Check if passed item is an object
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            object: function(item){
+
+            },
+            /**
+             * Check if passed item is an array
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            array: function(item){
+
+            },
+            /**
+             * Check if passed item is undefined
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            undefined: function(item){
+
+            },
+            /**
+             * Check if passed item is null
+             * @param  {*} item   Item to check against specified type
+             * @return {boolean}  Returns true if item is of specified type
+             */
+            null: function(item){
+
+            }
+        },
         /**
          * Takes two objects, one user-provided, and one of supported options with
          * default values. Returns object containing all supported options set to
@@ -33,12 +138,12 @@ var cafe = {
          *
          * @param  {*}  a                   Any variable to compare to b
          * @param  {*}  b                   Any variable to compare with a
-         * @param  {Boolean} [types=false]  compare data-types of variables, instead
+         * @param  {boolean} [types=false]  compare data-types of variables, instead
          *                                  of values. This is useful for making
          *                                  sure that merged values, using optionSupport
          *                                  for instance, are the correct data-types, if
          *                                  you don't care about the actual value.
-         * @param  {Boolean} [exists=false] Only for objects (including arrays).
+         * @param  {boolean} [exists=false] Only for objects (including arrays).
          *                                  Forces comparison of ONLY items with that
          *                                  exist (items with matching indexes). This
          *                                  means that when you use this parameter with
@@ -48,7 +153,7 @@ var cafe = {
          *                                  extraneous keys/indexes, those which do not
          *                                  exist in the paired object, will NOT prompt
          *                                  a false return.
-         * @return {Boolean}                False if discrepancies found between
+         * @return {boolean}                False if discrepancies found between
          *                                  'a' and 'b', in accordance with
          *                                  options set, True otherwise.
          */
@@ -140,56 +245,113 @@ var cafe = {
             return false;
         },
         /**
+         * Allows you to pass a node reference to remove from DOM
+         *
+         * @param  {HTMLElement} node Node reference to remove
+         * @return {boolean}          Returns true if successful, false otherwise
+         */
+        nodeRemove: function(node){
+            if(node && this.is.node(node, 1)){
+                node.parentNode.removeChild(node);
+                return true;
+            }
+            return false;
+        },
+        /**
          * Finds node on page, using reference, returns the node,
          * and deletes it from the DOM.
          *
-         * @param  {HTMLElement} reference   Node passed in to Extract
-         * @param  {Boolean} [deep=true]     Deep value to be passed to cloneNode().
-         *                                   Setting to false will only extract node
-         *                                   passed in, deleting children.
-         * @return {HTMLElement|Boolean}     Node extracted from DOM, false if node
-         *                                   not passed.
+         * @param  {HTMLElement} reference  Node passed in to Extract
+         * @param  {boolean} [deep=true]    Deep value to be passed to cloneNode().
+         *                                  Setting to false will only extract node
+         *                                  passed in, deleting children.
+         * @return {HTMLElement|Boolean}    Node extracted from DOM, false if node
+         *                                  not passed.
          */
         nodeExtract: function(reference, deep = true){
             let clone = false;
-            if(reference && reference.nodeType){
-
+            if(reference && this.is.node(reference)){
+                clone = reference.cloneNode(deep);
+            }
+            if(clone != false){
+                this.nodeRemove(reference);
             }
             return clone;
         },
-        nodeWrap: function(){
-
+        /**
+         * Takes a node and wraps it in a newly created parent. If the node is
+         * in the DOM (instead of simply stored in a variable), then nodeWrap()
+         * will place the new nodes back into the same DOM position. Otherwise,
+         * it simply returns the new nodes.
+         *
+         * @param  {HTMLElement} child              Child node to wraps
+         * @param  {string}      [parentTag='div']  String representing tag to wrap
+         *                                          around child. You may pass in
+         *                                          any value that's valid for
+         *                                          createElement().
+         * @return {HTMLElement|boolean}            Resulting element (Child wrapped
+         *                                          in new parent element), false
+         *                                          upon failure.
+         */
+        nodeWrap: function(child, parentTag = 'div'){
+            let newNode = false;
+            //If child is passed, and it's a node, and it's not in the DOM
+            if(child && this.is.node(child, 0)){
+                newNode   = document.createElement(parentTag);
+                childNode = newNode.appendChild(child);
+                return newNode;
+            //If child is passed, and it's a node, and it is in the DOM
+            } else if(child && this.is.node(child, 1)){
+                newNode   = document.createElement(parentTag);
+                this.putAfter(child, newNode);
+                newNode.appendChild(this.nodeExtract(child));
+                console.log("testing");
+            }
+            return newNode;
+        },
+        /**
+         * Inserts a node before another
+         *
+         * @param  {HTMLElement} existingNode Node that already exists in DOM
+         * @param  {HTMLElement} toInsert     Node to insert before existingNode
+         * @return {boolean}                  True if successful, false otherwise
+         */
+        putBefore: function(existingNode, toInsert){
+            if(existingNode && this.is.node(existingNode, 1)){
+                if(toInsert && this.is.node(toInsert)){
+                    existingNode.parentNode.insertBefore(toInsert, existingNode);
+                    return true;
+                }
+                return false
+            }
+            return false;
+        },
+        /**
+         * Cleverly runs putBefore() twice to insert the new node
+         * after the other.
+         *
+         * @param  {HTMLElement} existingNode Node that already exists in DOM
+         * @param  {HTMLElement} toInsert     Node to insert after existingNode
+         * @return {boolean}                  True if successful, false otherwise
+         */
+        putAfter: function(existingNode, toInsert){
+            if(this.putBefore(existingNode, toInsert)){
+                this.putBefore(toInsert, existingNode);
+                return true;
+            }
+            return false;
         },
         init: function(){
-            /*
-            if ( document.addEventListener ) {
-                Object.prototype.on = document.addEventListener;
-            } else if ( document.attachEvent ) {
-                Object.prototype.on = document.attachEvent;
-            }
-            Boolean.prototype.cafe = {
 
-            }
-            Number.prototype.cafe = {
-
-            }
-            String.prototype.cafe = {
-
-            }
-            Symbol.prototype.cafe = {
-
-            }
-            Object.prototype.cafe = {
-
-            }
-            */
+            //Populate now that the document has loaded
+            cafe.html = document.getElementsByTagName('html')[0];
 
             //Initialization message
             console.log("\n" +
                         "\n" +
                         "%c  (( %c    ___    \n" +
                         "%c   )) %c \\___/_  \n" +
-                        "%c  |~~| /~~~\\ \\  " +  "%c Café " + this.version + " - Part of the Café series \n" +
+                        "%c  |~~| /~~~\\ \\  " +  "%c Café Lounge " + this.version + " - Part of the Café series \n" +
                         "%c C|__| \\___/    " +  "%c by " + this.author + " - Initialized \n" +
                         "       `'`'`    \n" +
                         "\n" +
@@ -214,11 +376,15 @@ var cafe = {
          * that requires the use of Cafe methods, or access to loaded nodes.
          *
          * @param  {runOnLoad} callback Callback to execute when DOM has finished loading
-         * @return {Boolean}            Returns true once event listeners have been set
+         * @return {boolean}            Returns true once event listeners have been set
          */
         serve: function(callback){
-            this.init();
-            window.onload = callback;
+            window.onload = function(){
+                //Initialize some things after DOM has loaded
+                cafe.init();
+                //Run callback (all of user code)
+                callback();
+            }
             return true;
         }
 
